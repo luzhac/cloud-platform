be2a9fd8d40ce7fd41f45fa7aebb17c4ae558eae
luzhac <luzhac@gmail.com>
2025-11-02 17:43:22 +0000

    a

--- infra/terraform/modules/iam/main.tf
+++ infra/terraform/modules/iam/main.tf
@@ -21,6 +21,11 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "efs_csi_driver_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEFSCSIDriverPolicy"
+}
+
 resource "aws_iam_instance_profile" "profile" {
   name = "${var.cluster_name}-ssm-profile"
   role = aws_iam_role.ssm_role.name
